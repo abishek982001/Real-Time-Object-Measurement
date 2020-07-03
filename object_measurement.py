259984c063c10d69ed654694b5ef789ba63ef458
abishek982001 <abishek982001@gmail.com>
2020-07-03 13:44:37 +0530

    Modofed code to draw contours inside the things in A4 paper

--- object_measurement.py
+++ object_measurement.py
@@ -18,13 +18,14 @@ while True:
     else:
         img = cv2.imread(path)
 
-    img, finalContours = utils.getContours(img, minArea=50000, filter=4)
+    imgContours, finalContours = utils.getContours(img, minArea=50000, filter=4)
     if len(finalContours) != 0:
         biggest = finalContours[0][2]
         #print(biggest)
         imgWarp = utils.warpImage(img, biggest, widthOfPaper, heightOfPaper)
-        cv2.imshow("A4", imgWarp)
-
+        imgContours2, finalContours2 = utils.getContours(imgWarp, minArea=2000, filter=4, cannyThreshold=[50,50], draw=True)
+        cv2.imshow("A4", imgContours2)
+        
     img = cv2.resize(img, (0,0), None, 0.5, 0.5)
     cv2.imshow("Original", img)
     cv2.waitKey(1)
